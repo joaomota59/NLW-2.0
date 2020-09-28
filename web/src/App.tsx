0381e17e2cfa435fa0ab8526d8e210c312e28c4b
João Lucas Mota <joaomota59@hotmail.com>
2020-09-28 15:09:57 -0300

    alterações desenvolvimento web

---
+++ web/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Landing from './pages/Landing'
+
+import './assets/styles/global.css'
+//JSX = Javascript + XML
+
+function App() {
+  return (
+    <Landing />
+  );
+}
+
+export default App;
